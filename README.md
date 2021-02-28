b8b2bbf4084560f6e223c15cf66bdf7c1b8b341d
Joachim Tingvold <joachim@tingvold.com>
2021-02-28 22:43:43 +0100

    Add support for policies, and do more generic config fetch

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 
 PAN-OS configuration utility.
 
-Currently let's you fetch all "Objects" on one or more PANOS devices, and store
-them as YAML files. Useful for automating existing PANOS devices.
+Currently let's you fetch configuration from one or more PANOS devices. The
+configuration is stored as separate YAML files.
 
-### Current roadmap:
+## Features
+* Fetch configuration of the following types, and store as YAML:
+  - Objects (address, address groups, tags, etc)
+  - Policies (NAT rules, PBF, and security rules)
+
+## Roadmap:
 * Fetch other configuration aspects of PANOS devices, such as:
   - "Device" configuration
   - "Network" configuration
-  - "Policies" configuration
 * Push configuration to PANOS devices based on YAML
 * Push files to PANOS devices (certificates, etc)
 
-### Potential future roadmap:
+## Potential future roadmap:
 * Configure Panorama
 * Configure HA
 
--- modules/panos_utils.py
+++ modules/panos_utils.py
@@ -3,6 +3,7 @@
 import panos
 import panos.firewall
 import panos.objects
+import panos.policies
 
 class PanosUtils:
   def __init__(self, **kwargs):
@@ -10,11 +11,11 @@ class PanosUtils:
       for key, value in kwargs.items():
         setattr(self, key, value)
     
-  def connect_to_fw(self, args):
+  def connect_to_fw(self, hostname, args=None):
     try:
       fw = panos.firewall.Firewall(
-        hostname = args['hostname'],
-        api_key = self.utils.get_api_key(args['args'])
+        hostname = hostname,
+        api_key = self.utils.get_api_key(args)
       )
     except:
       self.utils.log.error("Could not connect to firewall.")
@@ -23,52 +24,63 @@ class PanosUtils:
       return fw
 
   def get_yaml_conf(self, force_overwrite):
-    for hostname, args in self.utils.config['panos']['hosts'].items():
-      conn = {
-        "hostname": hostname,
-        "args": args,
-        "add": False
-      }
-      fw = self.connect_to_fw(conn)
-      if fw is None:
-        continue
-      conn['fw'] = fw
+    fw_configs = self.get_configs_from_all_firewalls()
 
-      fw_config = self.get_fw_config_all(conn)
-
-      for config_type in fw_config:
-        for subconfig in fw_config[config_type]:
+    for hostname in fw_configs:
+      for config_type in fw_configs[hostname]['config']:
+        for config_child in fw_configs[hostname]['config'][config_type]:
           file_params = {
-            "filename": config_type + '_' + subconfig,
+            "filename": config_type + '_' + config_child,
             "force_overwrite": force_overwrite,
             "hostname": hostname
           }
-          self.utils.write_config_file(fw_config[config_type][subconfig],
-                                       file_params)
+          data = fw_configs[hostname]['config'][config_type][config_child]
+          self.utils.write_config_file(data, file_params)
 
-  def get_fw_config_all(self, conn):
+  def get_configs_from_all_firewalls(self):
+    fw_configs = {}
+    for hostname, args in self.utils.config['panos']['hosts'].items():
+      conn = {
+        "args": args,
+        "add": False
+      }
+      fw = self.connect_to_fw(hostname, conn['args'])
+      if fw is None:
+        continue
+      conn['fw'] = fw
+      conn['rulebase'] = panos.policies.Rulebase()
+      conn['fw'].add(conn['rulebase'])
+
+      fw_configs[hostname] = {}
+      fw_configs[hostname]['conn'] = conn
+      fw_configs[hostname]['config'] = self.get_config_types_from_firewall(conn)
+    return fw_configs
+
+  def get_config_types_from_firewall(self, conn):
     config_types = self.utils.api_params
     fw_config = {}
     for config_type in config_types:
-      fw_config[config_type] = self.get_fw_config(conn,
-                                                  config_types[config_type])
+      fw_config[config_type] = self.get_config_children_from_firewall(
+          conn, config_types[config_type])
     return fw_config
   
-  def get_fw_config(self, conn, configs):
+  def get_config_children_from_firewall(self, conn, config_type):
     fw_config = {}
-    for config_type in configs:
-      if configs[config_type]['skip']:
+    for config_child in config_type:
+      if config_type[config_child]['skip']:
         continue
 
-      config_info = configs[config_type]
+      config_info = config_type[config_child]
       config_class = self.utils.class_for_name(config_info['module'],
                                                config_info['class'])
-      data = self.get_config_from_fw(conn, config_info, config_class)
-      fw_config[config_type] = data
+      data = self.get_config_class_from_firewall(conn, config_info, 
+                                                 config_class)
+      fw_config[config_child] = data
     return fw_config
       
-  def get_config_from_fw(self, conn, config_info, config_class):
-    class_config = config_class.refreshall(conn['fw'], conn['add'])
+  def get_config_class_from_firewall(self, conn, config_info, config_class):
+    class_config = config_class.refreshall(conn[config_info['parent']],
+                                           conn['add'])
     
     data = []
     for obj in class_config:
